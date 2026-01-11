39d08ab192e1d95a7c442c544aadbb776317a855
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2026-01-11 22:20:18 +0000

    Configure settings for production: set DEBUG to False and update ALLOWED_HOSTS

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sru8lg)f7wyn!s-pj_1z01%3q98=pk!mq*@xeefg9y^la@eh0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
