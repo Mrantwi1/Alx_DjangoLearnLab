3d9d3728928cd4ea524362bd4db3e356e1b88e7b
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2026-01-11 21:59:25 +0000

    Fix: Implemented Register, Login, and Profile views

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,31 +1,28 @@
-# accounts/serializers.py
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'bio', 'profile_picture']
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture')
 
 class RegisterSerializer(serializers.ModelSerializer):
-    # The checker specifically looks for serializers.CharField() for passwords
     password = serializers.CharField()
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'bio', 'profile_picture']
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        # The checker looks for get_user_model().objects.create_user
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
+            email=validated_data.get('email'),
             password=validated_data['password'],
-            email=validated_data.get('email', ''),
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
+            bio=validated_data.get('bio'),
+            profile_picture=validated_data.get('profile_picture')
         )
         Token.objects.create(user=user)
         return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import UserSerializer, RegisterSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token.key
+        }, status=status.HTTP_201_CREATED)
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "token": token.key,
+            "user_id": user.pk,
+            "username": user.username
+        })
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    def get_object(self):
+        return self.request.user
