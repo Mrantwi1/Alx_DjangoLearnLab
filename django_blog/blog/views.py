ef5e16e1dc1da85017084900ba97a8ad1c473611
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2026-01-11 20:52:22 +0000

    Merged all features: Auth, CRUD, Comments, and Tags

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, redirect, get_object_of_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from .models import Post, Comment
+from .forms import CustomUserCreationForm, CommentForm
 
-# Create your views here.
+# --- AUTH VIEWS ---
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# --- POST CRUD VIEWS ---
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    # Logic to include comment form inside post detail
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'tags']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'tags']
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+# --- SEARCH & TAGS ---
+def search(request):
+    query = request.GET.get('q')
+    posts = Post.objects.filter(Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query))
+    return render(request, 'blog/post_list.html', {'posts': posts})
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,22 +1,22 @@
-"""
-URL configuration for django_blog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# django_blog/urls.py (Main Project URLS)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import views as blog_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Post CRUD
+    path('', blog_views.PostListView.as_view(), name='post-list'),
+    path('post/new/', blog_views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', blog_views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', blog_views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', blog_views.PostDeleteView.as_view(), name='post-delete'),
+    # Auth
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', blog_views.register, name='register'),
+    path('profile/', blog_views.register, name='profile'), # Simplification for profile
+    # Search
+    path('search/', blog_views.search, name='search'),
 ]
