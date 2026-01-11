be1fc731b5cda536ff9260d6e40168fcc20940b5
Mrantwi1 <kwameantwiboasiako020@gmail.com>
2026-01-11 20:38:43 +0000

    Configure database settings with USER and PORT for PostgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',
+        'USER': 'your_db_user',      # This is the 'USER' the check is looking for
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
